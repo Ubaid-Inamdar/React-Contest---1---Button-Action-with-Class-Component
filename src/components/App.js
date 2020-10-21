352e4ce3415c6dd9b28dcfee3d463800b97736a0
Ubaid-Inamdar <68742258+Ubaid-Inamdar@users.noreply.github.com>
2020-10-21 21:45:15 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    constructor(props) {
-		super(props);
-	};
-
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
-}
-
+const App = (props) => {
+  let [para, setpara] = useState(false);
+  const renderPargraph = () => {
+    setpara(!para);
+  };
+  return (
+    <div id="main">
+      {/* Do not remove this main div!! */}
+      <button id="click" onClick={renderPargraph}>
+        Click me
+      </button>
+      {para ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : null}
+    </div>
+  );
+};
 
 export default App;
-
